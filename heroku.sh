9243fe59f9724d78c62f997ffb5a97a79131ce92
Julian Bueno <julian.bueno@gmail.com>
2013-07-13 12:01:54 +1000

    Update heroku.sh

--- heroku.sh
+++ heroku.sh
@@ -1,13 +1,12 @@
 # Login and set up your SSH keys
-```sh
+
 heroku login
 ssh-keygen -t rsa
 heroku keys:add
-```
+
 # Clone a sample repo and push to heroku
-```sh
+
 git clone myrepo
 cd myrepodir
 #heroku create
 git push heroku master
-```
