c9b9a3a1ae1f7a51ee37caa5f591d26f72d53620
Julian Bueno <julian.bueno@gmail.com>
2013-07-30 20:13:22 +1000

    Added require and express

--- setup.sh
+++ setup.sh
@@ -26,6 +26,9 @@ nvm use v0.10.12
 # http://jshint.com/
 npm install -g jshint
 
+npm install express
+npm install require
+
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
